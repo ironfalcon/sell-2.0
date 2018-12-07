4249bc65bbd6e8f55ec2785ae9dc183778044708
ironfalcon <ironfalcon@yandex.ru>
2018-12-08 01:08:51 +0400

    add migration(client, product, order) add relationship

---
+++ database/migrations/2018_12_07_190304_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->string('surname', 30);
+            $table->integer('old');
+            $table->string('country', 30);
+            $table->string('region', 30);
+            $table->string('address');
+            $table->string('email');
+            $table->string('skype')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
