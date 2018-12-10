aed330168e5c3f0164c47c4d2dc3b323c0392209
ironfalcon <ironfalcon@yandex.ru>
2018-12-10 20:30:09 +0400

    add new css ad js bundle. edit layout/app add content to wlcome.blade.php

---
+++ public/js/active.js
@@ -0,0 +1,114 @@
+$(window).on("scroll", function() {
+    if ($(window).scrollTop() > 5 ) 
+        $(".logo").addClass("nav-logo-scroll");
+    else
+        $(".logo").removeClass("nav-logo-scroll");
+});
+
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+    // :: 1.0 Preloader Active Code
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut('slow', function () {
+            $(this).remove();
+        });
+    });
+
+    // :: 2.0 Nav Active Code
+    if ($.fn.classyNav) {
+        $('#lash-navbar').classyNav();
+    }
+
+    // :: 4.0 ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        browserWindow.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-angle-up"></i>'
+        });
+    }
+
+    // :: 5.0 CounterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    // :: 6.0 Sticky Active Code
+    if ($.fn.sticky) {
+        $(".lash-main-menu").sticky({
+            topSpacing: 0
+        });
+    }
+
+    // :: 7.0 Progress Bar Active Code
+    if ($.fn.circleProgress) {
+        $('#circle').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#cc1573',
+            thickness: '4',
+            reverse: true
+        });
+        $('#circle2').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#cc1573',
+            thickness: '4',
+            reverse: true
+        });
+        $('#circle3').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#cc1573',
+            thickness: '4',
+            reverse: true
+        });
+        $('#circle4').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#cc1573',
+            thickness: '4',
+            reverse: true
+        });
+    }
+
+    // :: 8.0 audioPlayer Active Code
+    if ($.fn.audioPlayer) {
+        $('audio').audioPlayer();
+    }
+
+    // :: 9.0 Tooltip Active Code
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip()
+    }
+
+    // :: 10.0 niceScroll Active Code
+    if ($.fn.niceScroll) {
+        $(".album-all-songs").niceScroll({
+            background: "#fff"
+        });
+    }
+
+    // :: 11.0 ScrollDown Active Code
+    $("#scrollDown").on('click', function () {
+        $('html, body').animate({
+            scrollTop: $("#about").offset().top - 85
+        }, 1500);
+    });
+
+    // :: 12.0 prevent default a click
+    $('a[href="#"]').on('click', function ($) {
+        $.preventDefault();
+    });
+
+    // :: 13.0 wow Active Code
+    if (browserWindow.width() > 767) {
+        new WOW().init();
+    }
+
+})(jQuery);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,94 +1,35 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-
-        <!-- jQuery library -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-        <!-- Popper JS -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-    </head>
-    <body>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-
-      <ul class="nav navbar-nav navbar-right">
-              @auth
-              <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                      {{ Auth::user()->name }} <span class="caret"></span>
-                  </a>
-
-                  <ul class="dropdown-menu">
-                      <li>
-                          <a href="{{ route('logout') }}"
-                              onclick="event.preventDefault();
-                                       document.getElementById('logout-form').submit();">
-                              Logout
-                          </a>
-
-                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                              {{ csrf_field() }}
-                          </form>
-                      </li>
-                  </ul>
-              </li>
-          @endauth
-      </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-            <div class="container">
-              <div>много всего<br>
-                текст работы и прочее
-              </div>
-              <button type="button" class="btn btn-success">Заказать</button>
+<!-- ##### About Us Area Start ##### -->
+<div id="about">
+    <div class="container-fluid">
+        <div class="d-flex justify-content-center row bd-highlight">
+            <div class="about-video col-12 col-sm-12 col-md-12 col-xl-6 bd-highlight">
+                    <iframe width="100%" height="450" src="https://www.youtube.com/embed/rtX7pZgiJe8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div class="about-block col-12 col-sm-12 col-md-12 col-xl-6 bd-highlight">
+                <div class="title-about text-center">Perfect School Lashes</div>
+                <p>
+                        Flex
+                        Quickly manage the layout, alignment, and sizing of grid columns, navigation,
+                        components, and more with a full suite of responsive flexbox utilities. For more complex implementations, custom CSS may be necessary.
+                </p>
+                <p>
+                        Flex
+                        Quickly manage the layout, alignment, and sizing of grid columns, navigation,
+                        components, and more with a full suite of responsive flexbox utilities. For more complex implementations, custom CSS may be necessary.
+                </p>
+                <p>
+                        Flex
+                        Quickly manage the layout, alignment, and sizing of grid columns, navigation,
+                        components, and more with a full suite of responsive flexbox utilities. For more complex implementations, custom CSS may be necessary.
+                </p>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+<!-- ##### About Us Area End ##### -->
+
+@endsection
