c89f848d070f97004202bbc8dd1882e50c6ce785
ironfalcon <ironfalcon@yandex.ru>
2018-12-11 19:30:50 +0400

    store methode logic (no complite)

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::post('/order/store', 'OrderController@store')->name('order.store');
+
 Route::get('/home', 'HomeController@index')->name('home');
